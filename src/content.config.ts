fba56adca08d0c504a02af12728a7d9537ada1c0
takeru-kusanagi <kusatake0929@gmail.com>
2026-07-07 18:47:26 +0900

    added access page

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
 import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { glob, file } from 'astro/loaders';
 
 const blog = defineCollection({
   loader: glob({ pattern: '**/*.md', base: './src/content/blog' }),
@@ -11,4 +11,25 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+const access = defineCollection({
+  loader: file('src/content/access/stores.json'),
+  schema: z.object({
+    storeName: z.string(),
+    photos: z.array(
+      z.object({
+        src: z.string(),
+        width: z.number(),
+        height: z.number(),
+      })
+    ),
+    postalCode: z.string(),
+    address: z.string(),
+    mapUrl: z.string(),
+    hours: z.string(),
+    closedDays: z.string(),
+    phone: z.string(),
+    reserveUrl: z.string(),
+  }),
+});
+
+export const collections = { blog, access };
